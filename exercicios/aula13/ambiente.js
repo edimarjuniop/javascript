fa3f8f8381b0433668f0c9cc495a2a61e63946a1
Edimar Junio Pereira dos Santos <145294819+edimarjuniop@users.noreply.github.com>
2023-11-22 15:42:32 -0300

    Create ambiente.js

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,11 @@
+// let index = 1
+// while (index <= 500) {
+//     console.log(`[${index}] Tudo bem?`);
+//     index++
+// }
+
+let index = 0
+do {
+    console.log(`[${index}] Tudo bem?`);
+    index++
+}while(index < 10)
