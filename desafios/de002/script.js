966e2d96d17477ec9f06fed4b89d64fd93f8780a
Edimar Junio Pereira dos Santos <145294819+edimarjuniop@users.noreply.github.com>
2023-11-22 22:24:30 -0300

    .

--- desafios/de002/script.js
+++ desafios/de002/script.js
@@ -1,11 +1,11 @@
 function cal() {
     let val = document.querySelector('input#num')
     let res = document.querySelector('select#table')
-    res.innerHTML = ''
 
     if (val.value.length == 0) {
         alert('Por favor, digite um número.')
     } else {
+        res.innerHTML = ''
         let v = Number(val.value)
         for (let i = 0; i <= 10; i++) {
             let item = document.createElement('option')
